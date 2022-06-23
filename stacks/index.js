2f9d270379e809d08f51817e3dacbfa51281a4e2
benjaminkorngiebel <bkorngie@stanford.edu>
2022-06-23 11:47:39 -0700

    Adding a storage stack

--- stacks/index.js
+++ stacks/index.js
@@ -1,12 +1,12 @@
-import MyStack from "./MyStack";
+import { StorageStack } from "./StorageStack";
 
 export default function main(app) {
-  // Set default runtime for all functions
   app.setDefaultFunctionProps({
-    runtime: "nodejs14.x"
+    runtime: "nodejs16.x",
+    srcPath: "services",
+    bundle: {
+      format: "esm",
+    },
   });
-
-  new MyStack(app, "my-stack");
-
-  // Add more stacks
+  app.stack(StorageStack);
 }
